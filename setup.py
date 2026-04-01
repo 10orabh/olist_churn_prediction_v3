fe3af64666c4b1635ced2be17aff95fc2fea56ed
10orabh <123290989+10orabh@users.noreply.github.com>
2026-04-02 00:30:39 +0530

    made project structure with env

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Sourabh Pal",
+    description="This tool helps you to predict which customer will not buy product in 120 days after purchase on time.",
+    packages=find_packages(),
+    
+)
